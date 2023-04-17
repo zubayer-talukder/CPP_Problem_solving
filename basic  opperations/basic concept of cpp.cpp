5523931b2db4d926da8482bc5c35831618325cf7
Abdullah Zubayer Talukder <121654019+zubayer-talukder@users.noreply.github.com>
2023-04-18 00:09:55 +0600

    Add files via upload

---
+++ basic  opperations/basic concept of cpp.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main (){
+    cout<<"My name is zubayer\n ";
+
+cout<< "Talukder"<<endl;
+cout<<"Abdullah "<<endl ;
+//endl (Space Sequence) refers to create a ENTER  on KeyBoard  . You can use "\n"   
+
+}
---
+++ basic  opperations/printing number.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<4<<endl;
+    cout<<5+4<<endl;
+    // where 5+4 is considered as an integer
+
+    cout<<"5+9"<<endl;
+    //here 5+9 is considered as a TEXT. it means that inside the invited comma any thing is considered as  a TEXT ""
+}
