b3d73c00e30646512d856c6f6c59b8436cb563f5
Abdullah Zubayer Talukder <121654019+zubayer-talukder@users.noreply.github.com>
2023-06-28 10:33:01 +0600

    Add files via upload

---
+++ 10. data Type Limits/BOOLEAN+Type+data+limits.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main()
+{
+    bool x = true;
+    cout << x << endl;
+    bool y = 12;
+    cout << x << endl; 
+   // any non zero number will be considered as a 1
+   bool z =0;
+   cout << z;
+   // any zero number will be zero 
+    return 0;
+}
---
+++ 10. data Type Limits/data+limits+integer+Type.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+ int main(){
+    int a=INT_MAX;
+    
+    cout << a << endl;
+    // 2147483647 is the Highest limit of int data type
+    // int data type can't hold more than it's limits
+
+    int b =INT_MIN;
+    cout << b << endl;
+ }
