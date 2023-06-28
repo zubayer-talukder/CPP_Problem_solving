237cbf3ec25bba5b3b52bcd19b85ebe6ac7edc9b
Abdullah Zubayer Talukder <121654019+zubayer-talukder@users.noreply.github.com>
2023-06-28 10:34:43 +0600

    Add files via upload

---
+++ 13. taking inputs from users/square+of+number.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int x;
+    cout<< "Enter a Number : ";
+    cin >> x;
+    cout <<"Square Of Number is : "<< x * x;
+}
---
+++ 13. taking inputs from users/sumOf2number.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int x , y;
+    cout<< "Enter First Number : ";
+    cin >> x ;
+    cout<< "Enter Second Number : ";
+    cin >> y ;
+cout << "Your result is : " << x + y;
+}
