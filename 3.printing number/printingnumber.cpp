de7841a0b8968e95ef07e9f24e7ebef55be1009d
Abdullah Zubayer Talukder <121654019+zubayer-talukder@users.noreply.github.com>
2023-06-28 10:29:57 +0600

    Add files via upload

---
+++ 3.printing number/printingnumber.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<< 87<<endl;
+// here 87 is a integer
+cout<< 7+5<<endl;
+//here 7+5 is considered also as a integer
+cout<<"7+4"; // ⇒ is considered as a text in double Quatation
+}
