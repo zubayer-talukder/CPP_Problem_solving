742750a60d153ef2ef043c0b4a680c41dd53b208
Abdullah Zubayer Talukder <121654019+zubayer-talukder@users.noreply.github.com>
2023-06-28 10:33:16 +0600

    Add files via upload

---
+++ 11. variable naming rules/variable+naming+rules.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // 1. Variable can start from an alphabet or underscore _ or $
+   int _dog = 54;
+    cout << _dog << endl;
+
+    int $mia = 44;
+    cout << $mia<< endl;
+
+    // 2. special characters except _ and $ are not allowed.
+        //    int x# =23;
+        //    cout<<x#;
+
+    // 3. some particular keywords are not allowed
+            // 1.auto 2.double 3. int 4. break 5.extern 6.enum
+            // 7.unsigned 8.while 9.case 10.sizeof 11. for 12.const
+            // 13.static 14. long 15.continue 16.float 17.else 18.signed
+            // 19.do 20.short 21. switch 22. char 23.volatile 24. default
+            // 25. goto 26.struct 27.if 28.union 29. return 30.void 
+            // 31.register 32.typedef
+            
+    // 4. commas or blanks are not allowed in naming variable
+                // int zubayer mia =32;
+                // cout << zubayer mia;
+
+    
+}
