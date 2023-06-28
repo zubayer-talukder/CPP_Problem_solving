e4f186ea99e35f29536dc15806e29fef769f1fe1
Abdullah Zubayer Talukder <121654019+zubayer-talukder@users.noreply.github.com>
2023-06-28 10:29:11 +0600

    Add files via upload

---
+++ 1. Hello CPP/hello.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std; 
+
+int main() {
+
+    cout << "Hello World! ZUBAYER."<<endl;
+    cout<<endl;
+    cout<< "Hello Mr Beast. \n ";
+    return 0;
+}
---
+++ 1. Hello CPP/understanding+namespace+std.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+//using namespace std;
+int main()
+{
+   std :: cout<< "Hello ";
+   // we can Use "std :: ......" instead of "using namespace std ;" 
+   // but we have to apply "std :: " in every line. For skip this lengthy process we use 
+   // using namespace std; command
+   std :: cout <<"Mr. Beast";
+
+   // iostream is a library function
+}
